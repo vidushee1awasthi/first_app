98d0c82750c9af57c197ae017419bd0928b37f7c
vidushee <vidusheeawasthi19@gmail.com>
2012-04-19 16:19:19 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidushee::Application.config.secret_token = '1298b31007dffc9b93cd8400ef72ce1cb86d8d2fa543d675e50fab3c5a366404a1d0e4501868cb68a9674a2247c82d3e1f824e5a0367e18d26563d14d0722955'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Vidushee::Application.config.session_store :cookie_store, key: '_vidushee_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Vidushee::Application.config.session_store :active_record_store
